7ac18c3d4029de034a09ec6dcf6449613cc71ed9
Roberto Bradley <brad7746@m506-118-26433.local>
2018-08-31 09:43:25 -0700

    first push for tip calc

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Roberto Bradley on 8/29/18.
+//  Copyright © 2018 Roberto Bradley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%f" arguements: CVarArgType...)
+        totalLabel.text = "$\(total)"
+    }
+    
+}
+
